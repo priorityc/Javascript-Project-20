eb386a0ad4ed0a1b130cc9f8cce221b263cb96b9
priorityc <petya83dimitrova@gmail.com>
2022-02-28 11:07:19 +0000

    first commit

---
+++ project20.js
@@ -0,0 +1,44 @@
+document.querySelector("h1").style.color = "green";
+//change text
+document.querySelector("h1").innerHTML = 'Hello';
+//alert
+  let alert= document.querySelector('h1').innerHTML = ('Hello');
+//THIS IS TASK TWO
+ /* window.alert(('h1').innerHTML=('Hello'));
+  
+  //display current date
+const date = new Date();
+ document.getElementById('date').innerHTML = date;
+
+ //set date birthday
+
+ const birthday = new Date();
+ const month = new Date();
+ const year = new Date();
+
+ birthday.setDate(30);
+ month.setDate(11);
+ year.setDate(1983);
+ console.log(birthday);
+
+ document.getElementById("Birth-day").innerHTML = birthday;*/
+  
+ //coment in the code above to see the effect UTC
+const  Localdate = new Date();
+
+const date = Localdate.toUTCString();
+document.getElementById('date').innerHTML = date;
+
+//TASK 3
+
+ 
+
+answer = prompt('Please tell me what is your favorite colour?');
+  if(answer.includes(answer.toLowerCase())=confirm) {
+    arrowExp6 = () => {
+      /*let newcolor = (answer);*/
+    document.querySelector('body').style.color;
+      }
+
+    }
+
